77c6e03d38f001380b6dfa61fb7068fa44e9fba1
Padmasripriya <padmasripriya17@gmail.com>
2023-08-16 17:34:46 -0400

    added footer changes

--- src/modules/CTAComponent.js
+++ src/modules/CTAComponent.js
@@ -3,8 +3,8 @@ import React from "react";
 export function CTAComponent() {
     return(
         <div className="cta-component container">
-            <h3>Boost your links today</h3>
-            <p><a href="">Get Started</a></p>
+            <h3 className="mb-3">Boost your links today</h3>
+            <p className="cta-button"><a href="">Get Started</a></p>
         </div>
     );
 }
--- src/modules/ThreeColumnComponent.js
+++ src/modules/ThreeColumnComponent.js
@@ -1,34 +1,43 @@
 import React from "react";
+import BrandRecognitionIcon from "../images/icon-brand-recognition.svg";
+import DetailedRecordsIcon from "../images/icon-detailed-records.svg";
+import FullyCustomizableIcon from "../images/icon-fully-customizable.svg";
 
 export function ThreeColumnComponent() {
     return(
         <div className="three-column-component gray container">
             <div class="row">
                 <div class="col">
-                    <img src="" alt="" />
-                    <h4>Brand Recognition</h4>
-                    <p>
-                        Boost your brand recognition with each click.
-                        Generic links don't mean a thing. Branded links
-                        help instil confidence in your content.
-                    </p>
+                    <div className="first-column">
+                        <img src={BrandRecognitionIcon} alt="brand recognition icon" />
+                        <h4>Brand Recognition</h4>
+                        <p>
+                            Boost your brand recognition with each click.
+                            Generic links don't mean a thing. Branded links
+                            help instil confidence in your content.
+                        </p>
+                    </div> 
                 </div>
                 <div class="col">
-                    <img src="" alt="" />
-                    <h4>Detailed Records</h4>
-                    <p>
-                        Gain insights into who is clicking your likes.
-                        Knowing when and where people engage with your content 
-                        helps inform better decisions.
-                    </p>
+                    <div className="second-column">
+                        <img src={DetailedRecordsIcon} alt="detailed records" />
+                        <h4>Detailed Records</h4>
+                        <p>
+                            Gain insights into who is clicking your likes.
+                            Knowing when and where people engage with your content 
+                            helps inform better decisions.
+                       </p>
+                    </div>
                 </div>
                 <div class="col">
-                    <img src="" alt="" />
-                    <h4>Fully Customizable</h4>
-                    <p>
-                        Improve brand awareness and content discoverability through 
-                        customizable links, supercharging audience engagement.
-                    </p>
+                    <div className="third-column">
+                        <img src={FullyCustomizableIcon} alt="fully customizable icon" />
+                        <h4>Fully Customizable</h4>
+                        <p>
+                            Improve brand awareness and content discoverability through 
+                            customizable links, supercharging audience engagement.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
