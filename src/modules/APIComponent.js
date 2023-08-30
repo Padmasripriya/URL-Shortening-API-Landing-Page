ce6955ba7c0e372379a319ee95cf9bad682c3ad0
Padmasripriya <padmasripriya17@gmail.com>
2023-08-30 16:15:41 -0500

    added css to the api component

--- src/modules/APIComponent.js
+++ src/modules/APIComponent.js
@@ -3,8 +3,10 @@ import React from "react";
 export function APIComponent() {
     return(
         <div className="api-component container">
-            <input className="input-url mb-5" type="url" name="url" placeholder="Shorten a link here..."/>
-            <input className="input-url-button" type="button" name="shorten-url" value="Shorten It!" />
+            <div className="api-inputs px-3">
+                <input className="input-url w-75 mb-5" type="url" name="url" placeholder="Shorten a link here..."/>
+                <input className="input-url-button mx-3" type="button" name="shorten-url" value="Shorten It!" />
+            </div>
         </div>
     );
 }
